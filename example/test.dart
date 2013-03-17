b7a7601bb2b3ca8de0f7cec3f219ac5ceebfd3b1
Joseph Moniz <joseph.moniz@gmail.com>
2013-03-17 16:51:14 -0700

    created example

---
+++ example/test.dart
@@ -0,0 +1,24 @@
+import 'package:event_emitter/event_emitter.dart';
+
+main() {
+
+  var events  = new EventEmitter();
+  var handler = (v) => print("*** called handler");
+
+  events.emit("boom", "some data");
+  print("should not have called handler.");
+
+  events.on("boom", handler);
+  events.emit("boom", "some data");
+  print("should have called handler.");
+
+  events.off("boom", handler);
+  events.emit("boom", "some data");
+  print("should not have called handler.");
+
+  events.on("boom", handler);
+  events.clearListeners();
+  events.emit("boom", "some data");
+  print("should not have called handler.");
+
+}
